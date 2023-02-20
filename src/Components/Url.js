4dae5525e6657671128e4686ffbb0fe9f015b6d2
Hassan Siddiqui <hassansiddiqui7865@gmail.com>
2023-02-21 00:47:38 +0500

    new

--- src/Components/Url.js
+++ src/Components/Url.js
@@ -4,7 +4,7 @@ const User = JSON.parse(localStorage.getItem("persist:root"))?.currentuser
 const currentuser = User && JSON.parse(User)
 const token = currentuser?.token
 console.log(token)
-const BASE_URL = "http://localhost:5500/api"
+const BASE_URL = "https://sore-puce-squid-cape.cyclic.app/api"
 export const Req = axios.create({
     baseURL:BASE_URL,
     headers:{token:`Bearer ${token}`}
